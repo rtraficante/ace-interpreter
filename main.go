556325bf8fddb947cfcd7fb6c04390bedfd0e5ea
RTraficante <rtraficante22@gmail.com>
2023-08-03 20:40:58 -0400

    parsing let statement functionality

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/rtraficante/ace-interpreter/repl"
+)
+
+func main() {
+    user, err := user.Current()
+
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Hello %s! This is the Ace programming language\n", user.Username)
+    fmt.Printf("Feel free to type in commands\n")
+
+    repl.Start(os.Stdin, os.Stdout)
+}
